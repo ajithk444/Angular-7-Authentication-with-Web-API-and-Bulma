b68c7bcc1df30b9011f9572b1b2d7730cd2d1848
ajithk444 <Rasalam@123>
2019-03-19 17:24:54 +0530

    commit

--- angular-bulma-app/src/app/app.module.ts
+++ angular-bulma-app/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
     ProjectsComponent
   ],
   imports: [
